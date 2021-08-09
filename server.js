02aca3f00c72a3e264efb7e683125f1679ae63c4
shirley he <HEX10@miamioh.edu>
2021-08-09 17:17:49 -0400

    change to http not https

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 function requireHTTPS(req, res, next) {
     // The 'x-forwarded-proto' check is for Heroku
     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
+        return res.redirect('http://' + req.get('host') + req.url);
     }
     next();
 }
