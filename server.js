269432c7f16c5ca4e360a05f4ed1815c17cabf25
shirley he <HEX10@miamioh.edu>
2021-08-09 16:48:14 -0400

    update comments and dist folder path

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Redirect all traffic from http to https
 function requireHTTPS(req, res, next) {
     // The 'x-forwarded-proto' check is for Heroku
     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
@@ -10,10 +11,12 @@ const express = require('express');
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static('./dist/angular-heroku'));
+// Serve our static files
+app.use(express.static('./dist/CCVGproject'));
 
+//Wait for a request to any path and redirect all of the requests to index.html.
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/angular-heroku/' }),
+    res.sendFile('index.html', { root: 'dist/CCVGproject/' }),
 );
-
+// Listen for requests at the PORT specified by env variables or the default Heroku port, which is 8080.
 app.listen(process.env.PORT || 8080);
